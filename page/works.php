b8514ec91d445e2d752603a980a505a71e7597a6
ghevp <gentont@icloud.com>
2023-04-08 12:48:03 +0900

    画像関係

--- page/works.php
+++ page/works.php
@@ -20,5 +20,30 @@
         <h2>タイプライター</h2>
         <p><a href="https://amzn.to/3DY7BIq">レゴ21327</a>のタイプライターを組み立てたのもです</p>
         <img src="img/works/180214.jpg">
+        <!--これより先2023年度更新 -->
+        <h2>クレーン</h2>
+
+        <p>大規模な工事現場などで活躍する超大型クローラークレーンを作りました。ラジコンで操作ができ、フックや2本の鉄骨部分は全て独立して動くようになっています。自走も可能な設計になっています。高さは最大で1.6メートルほどもあります！</p>
+        <img src="img/works/crane.jpg">
+        <h2>バス</h2>
+
+        <p>東急バスの車両をモチーフに路線バスを作りました。ラジコンで操作ができます。また空気圧シリンダーを利用したドア開閉ギミックも搭載しています。</p>
+        <img src="img/works/lego_tokyubus.jpg">
+        <h1>GBC</h1>
+        <p>GBCとはThe Great Ball Contraptionの略で、ボール運搬装置を各々が製作し、それをつなげて遊ぶというものです。
+            (<a href="http://legokarakuri.blog91.fc2.com/blog-entry-26.html">LEGO TECHNIC からくり部屋</a> より引用)</p>
+
+       <h2> GBC</h2>
+
+        <p>Riku Katsumata氏のGBC作品である“Hybrid double lifter”を部員が真似して組み立てたものですが、一部の機構は組み立てた部員のオリジナルのものとなっています。因みにこのGBCが他の人によって組み立てられたのは初めてだそうです。</p>
+        <img src="img/works/katsumata_gbc.jpg">
+        <h2>部長のGBC</h2>
+
+        <p>お椀の形のパーツに入ったボールを、カタパルトの仕組みを用いて出口まで飛ばすGBCです。周期的で軽快な発射音が魅力です。</p>
+        <img src="img/works/SR_gbc.jpg">
+        <h2>池上さんのGBC</h2>
+
+        <p>クランク機構によって生み出された往復円運動を利用して、ボールをバケツリレーのように送り出す仕組みのGBCです。独特な動きが見ていてとても面白いです。</p>
+        <img src="img/works/ikegami_gbc.jpg">
     </div>
 </main>
