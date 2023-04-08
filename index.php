31ac6154833e32af3c1c58fcd75b069a04ce85be
ghevp <gentont@icloud.com>
2023-04-08 13:17:10 +0900

    カスの＞を削除

--- index.php
+++ index.php
@@ -36,39 +36,33 @@ $t = 0;
     <meta property="og:url" content="https://shizudailego.com">
     <meta property="og:title" content="浜松学生レゴサークル">
     <meta name="og:description" content="浜松を中心に活動している学生主体のレゴサークルです！！">
-    <<link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@500&family=Sawarabi+Gothic&family=Shippori+Mincho+B1:wght@800&family=Yuji+Syuku&display=swap" rel="stylesheet">
-        <link rel="icon" href="img/logo.png" type="image/x-icon">
-        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
-        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Kosugi&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="css/style.css">
-        <!--後でphpのif文の作成-->
-        <title>
-            <?php
-            if (isset($title) == "") {
-                echo "浜松学生レゴサークル";
-            } else {
-                echo "" . $title[$page] . " - 浜松学生レゴサークル";
-            }
-            ?>
-        </title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@500&family=Sawarabi+Gothic&family=Shippori+Mincho+B1:wght@800&family=Yuji+Syuku&display=swap" rel="stylesheet">
+    <link rel="icon" href="img/logo.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kosugi&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <!--後でphpのif文の作成-->
+    <title>
+        <?php
+        if (isset($title) == "") {
+            echo "浜松学生レゴサークル";
+        } else {
+            echo "" . $title[$page] . " - 浜松学生レゴサークル";
+        }
+        ?>
+    </title>
 
 <body style="margin: 0;">
     <?php
-
-
     include('page/' . $page . ".php");
-
-
-
-
     ?>
     <header class="header">
         <div class="header_inner">
